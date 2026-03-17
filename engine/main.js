24e0016446d351934dacb1ff66e51b169b497fb6
Empresastar <gemadotopmatheus@gmail.com>
2026-03-16 22:30:28 -0300

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -3,39 +3,27 @@ import { scene } from './state.js';
 import { camera, movePlayer } from './player.js';
 import { updateVehicle, isInsideVehicle, spawnCar } from './vehicle.js';
 import './world.js';
-import './combat.js'; // ISSO ATIVA O SISTEMA DE ARMAS
+import './combat.js';
 
-// 1. RENDERIZADOR
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
 document.getElementById('canvas-container').appendChild(renderer.domElement);
 
-// 2. ILUMINAÇÃO
 scene.add(new THREE.AmbientLight(0xffffff, 0.6));
 const sun = new THREE.DirectionalLight(0xffffff, 1);
 sun.position.set(10, 20, 10);
 scene.add(sun);
 
-// 3. SPAWN DO CARRO
 spawnCar(-10, -10);
 
-// 4. LOOP PRINCIPAL (FRAME POR FRAME)
 function animate() {
     requestAnimationFrame(animate);
-
-    if (isInsideVehicle) {
-        updateVehicle(); // Se estiver no carro
-    } else {
-        movePlayer();    // Se estiver a pé (inclui pulo e rolamento)
-    }
-
+    if (isInsideVehicle) updateVehicle();
+    else movePlayer();
     renderer.render(scene, camera);
 }
-
 animate();
 
-// AJUSTE DE TELA
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
